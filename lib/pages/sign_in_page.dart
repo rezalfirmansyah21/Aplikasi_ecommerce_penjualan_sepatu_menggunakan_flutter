e9f29087aec7186f5ccd80cd56f17b145f2fb812
Rezalfirmansyah21 <rezalfirmansyah21@gmail.com>
2021-12-05 20:39:23 +0700

    first commit

---
+++ lib/pages/sign_in_page.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shamo/providers/auth_provider.dart';
+import 'package:shamo/theme.dart';
+import 'package:shamo/widgets/loading_button.dart';
+
+class SignInPage extends StatefulWidget {
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  TextEditingController emailController = TextEditingController(text: '');
+
+  TextEditingController passwordController = TextEditingController(text: '');
+
+  bool isLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    AuthProvider authProvider = Provider.of<AuthProvider>(context);
+
+    handleSignIn() async {
+      //mulai loading
+      setState(() {
+        isLoading = true;
+      });
+
+      if (await authProvider.login(
+        email: emailController.text,
+        password: passwordController.text,
+      )) {
+        Navigator.pushNamed(context, '/home');
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: alertColor,
+            content: Text(
+              'Login tidak berhasil!',
+              textAlign: TextAlign.center,
+            ),
+          ),
+        );
+      }
+      //selesai loading
+      setState(() {
+        isLoading = false;
+      });
+    }
+
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+          top: 50,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Login',
+              style: primaryTextStyle.copyWith(
+                fontSize: 24,
+                fontWeight: semiBold,
+              ),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Text(
+              'Sign In to Continue',
+              style: subtitleTextStyle,
+            ),
+          ],
+        ),
+      );
+    }
+
+    //email
+    Widget emailInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 70),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Email Address',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/icon_email.png',
+                      width: 17,
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        controller: emailController,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Your Email Address',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    //password
+    Widget passwordInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Password',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: medium,
+              ),
+            ),
+            SizedBox(
+              height: 12,
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              decoration: BoxDecoration(
+                color: backgroundColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/icon_password.png',
+                      width: 17,
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        obscureText: true,
+                        controller: passwordController,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Your Password',
+                          hintStyle: subtitleTextStyle,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    // ignore: non_constant_identifier_names
+    Widget SignInButton() {
+      return Container(
+        height: 50,
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 30),
+        child: TextButton(
+          onPressed: handleSignIn,
+          style: TextButton.styleFrom(
+              backgroundColor: primaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              )),
+          child: Text(
+            'Sign In',
+            style: primaryTextStyle.copyWith(
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget footer() {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Don\'t have an account? ',
+              style: subtitleTextStyle.copyWith(
+                fontSize: 12,
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/sign-up');
+              },
+              child: Text(
+                'Sign Up',
+                style: purpleTextStyle.copyWith(
+                  fontSize: 12,
+                  fontWeight: medium,
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      resizeToAvoidBottomInset: false,
+      body: SingleChildScrollView(
+        //utk keyboard agar menyesuaikan
+        reverse: true,
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: defaultMargin,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              header(),
+              emailInput(),
+              passwordInput(),
+              isLoading ? LoadingButton() : SignInButton(),
+              //padding utk penyesuaian keyboard
+              Padding(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+              ),
+              footer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/providers/cart_provider.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/models/cart_model.dart';
+import 'package:shamo/models/product_model.dart';
+
+class CartProvider with ChangeNotifier {
+  List<CartModel> _carts = [];
+
+  List<CartModel> get carts => _carts;
+
+  set carts(List<CartModel> carts) {
+    _carts = carts;
+    notifyListeners();
+  }
+
+  addCart(ProductModel product) {
+    if (productExist(product)) {
+      int index =
+          _carts.indexWhere((element) => element.product.id == product.id);
+      //jika product nya sama hanya quantity nya yg nambah
+      _carts[index].quantity++;
+    } else {
+      _carts.add(
+        CartModel(
+          id: _carts.length,
+          product: product,
+          quantity: 1,
+        ),
+      );
+    }
+
+    notifyListeners();
+  }
+
+  removeCart(int id) {
+    //meremove dari id yg diambil
+    _carts.removeAt(id);
+    notifyListeners();
+  }
+
+  addQuantity(int id) {
+    _carts[id].quantity++;
+    notifyListeners();
+  }
+
+  reduceQuantity(int id) {
+    //mengurangi quantity
+    _carts[id].quantity--;
+    //pengkonidisian agar tidak minus
+    if (_carts[id].quantity == 0) {
+      _carts.removeAt(id);
+    }
+    notifyListeners();
+  }
+
+  totalItems() {
+    int total = 0;
+    for (var item in _carts) {
+      total += item.quantity;
+    }
+
+    return total;
+  }
+
+  totalPrice() {
+    double total = 0;
+    for (var item in _carts) {
+      total += (item.quantity * item.product.price);
+    }
+    return total;
+  }
+
+  productExist(ProductModel product) {
+    if (_carts.indexWhere((element) => element.product.id == product.id) ==
+        -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
